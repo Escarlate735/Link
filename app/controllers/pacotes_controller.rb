363e6227daba53f25904949bb3e133fcb5750906
Escarlate735 <gusfalo@outlook.com.br>
2018-06-27 23:56:02 -0300

    Essa versão tem tudo que tá no Getting Started do rails guides, exceto a senha e a opção de acrescentar elementos à model secundária (de pagamento)

---
+++ app/controllers/pacotes_controller.rb
@@ -0,0 +1,19 @@
+class PacotesController < ApplicationController
+  def create
+    @cliente = Cliente.find(params[:cliente_id])
+    @pacote = @cliente.pacotes.create(pacote_params)
+    redirect_to cliente_path(@cliente)
+  end
+ 
+  def destroy
+  	@cliente = Cliente.find(params[:cliente_id])
+  	@pacote = @cliente.pacotes.find(params[:id])
+  	@pacote.destroy
+  	redirect_to cliente_path(@cliente)
+  end
+
+  private
+    def pacote_params
+      params.require(:pacote).permit(:Pagamento, :Plano)
+    end
+end
