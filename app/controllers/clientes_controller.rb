e71d96ca91ea73a798908ec9d5bbfc0b013c532c
Escarlate735 <gusfalo@outlook.com.br>
2018-06-27 21:31:15 -0300

    V2

---
+++ app/controllers/clientes_controller.rb
@@ -0,0 +1,49 @@
+class ClientesController < ApplicationController
+  def index
+    @clientes = Cliente.all
+  end
+
+  def show
+    @cliente = Cliente.find(params[:id])
+  end
+
+  def new
+    @cliente = Cliente.new
+  end
+
+  def edit
+    @cliente = Cliente.find(params[:id])
+  end
+
+  def create
+    @cliente = Cliente.new(cliente_params)
+
+    if @cliente.save
+    redirect_to @cliente
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @cliente = Cliente.find(params[:id])
+
+    if @cliente.update(cliente_params)
+      redirect_to @cliente
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @cliente = Cliente.find(params[:id])
+    @cliente.destroy
+
+    redirect_to clientes_path
+  end
+
+  private
+  def cliente_params
+    params.require(:cliente).permit(:Nome, :CPF, :Email, :Endereco, :Data_de_Nascimento, :Status)
+  end
+end
